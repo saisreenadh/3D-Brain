c88d34abe81637f1772f3d6d8d3fc1ad9cb32a94
Minoo Jang <minoojang@DN51vd66.SUNet>
2024-11-06 20:01:45 -0800

    show plots in tutorial

--- Tutorials/1_Analysis_Overview.py
+++ Tutorials/1_Analysis_Overview.py
@@ -4,7 +4,9 @@
 
 import numpy as np
 import mne
-
+import matplotlib.pyplot as plt
+mne.set_config('MNE_PLOT_BACKEND', 'matplotlib')
+plt.ion()
 
 ### LOAD DATA ###
 
@@ -21,15 +23,51 @@ raw = mne.io.read_raw_fif(sample_data_raw_file)
 # print(raw.info)
 
 raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)    # low pass filter, power spectral density graph
-raw.plot(duration=5, n_channels=30)     # plot 30 channels' raw data over 5 seconds
+fig = raw.plot(duration=5, n_channels=30)     # plot 30 channels' raw data over 5 seconds
 
 # explore future pre-processing techniques: maxwell filtering, signal-space projection, independent components analysis, filtering, downsampling
 # read mne.preprocessing, mne.filter documentation
 
 
+### MODELING ###
 
 # set up and fit the ICA
 ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
 ica.fit(raw)
 ica.exclude = [1, 2]  # details on how we picked these artifacts are omitted in this tutorial
-ica.plot_properties(raw, picks=ica.exclude)
+# ica.plot_properties(raw, picks=ica.exclude)
+
+
+orig_raw = raw.copy()
+raw.load_data() # loads data into memory
+ica.apply(raw) # applies pre-processing, artifact removal to data
+
+# show some frontal channels to clearly illustrate the artifact removal
+chs = [
+    "MEG 0111",
+    "MEG 0121",
+    "MEG 0131",
+    "MEG 0211",
+    "MEG 0221",
+    "MEG 0231",
+    "MEG 0311",
+    "MEG 0321",
+    "MEG 0331",
+    "MEG 1511",
+    "MEG 1521",
+    "MEG 1531",
+    "EEG 001",
+    "EEG 002",
+    "EEG 003",
+    "EEG 004",
+    "EEG 005",
+    "EEG 006",
+    "EEG 007",
+    "EEG 008",
+]
+chan_idxs = [raw.ch_names.index(ch) for ch in chs]
+
+orig_raw.plot(order=chan_idxs, start=12, duration=4)
+raw.plot(order=chan_idxs, start=12, duration=4)
+
+plt.show(block=True)
